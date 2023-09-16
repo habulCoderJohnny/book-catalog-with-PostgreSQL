2a6ddf8d29941c464f22ca4114a6d80a348d9cdb
habulCoderJohnny <fokruljohnny@gmail.com>
2023-09-16 21:25:56 +0600

    Auth route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { UserRoutes } from '../modules/user/routes';
+import { AuthRoutes } from '../modules/auth/routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
+  {
+    path: "/auth",
+    routes: AuthRoutes
+  },
   {
     path: '/users',
     routes: UserRoutes,
